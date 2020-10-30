88a080f1ecbc26b67c9aa2d70b16350917c3f2fb
Oleksii Bardin <bardincom@icloud.com>
2020-10-30 09:45:03 +0200

    Basic project configuration

---
+++ Weather/Weather/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Aleksey Bardin on 30.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        assembly()
+        return true
+    }
+
+}
+
+private extension AppDelegate {
+
+    func assembly() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let rootViewController = ViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Aleksey Bardin on 30.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .red
+  }
+
+
+}
+
